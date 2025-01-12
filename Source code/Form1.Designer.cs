ff14e3f10d93c60f947626c4ce59115231135b6e
Boteteh Labs <botetehlabs@gmail.com>
2025-01-12 20:41:20 +0200

    Initial commit

---
+++ Source code/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Keybind_Solution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Record Hotkey";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hotkey";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "NA";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(13, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Keys to be pressed";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Key # 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(18, 156);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Key # 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(18, 185);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Key # 3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(129, 124);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "NA";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(129, 157);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "NA";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(129, 187);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "NA";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(144)))), ((int)(((byte)(144)))));
+            this.button5.Location = new System.Drawing.Point(235, 124);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(24, 20);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "X";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(144)))), ((int)(((byte)(144)))));
+            this.button6.Location = new System.Drawing.Point(235, 156);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(24, 20);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "X";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(144)))), ((int)(((byte)(144)))));
+            this.button7.Location = new System.Drawing.Point(235, 188);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(24, 20);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "X";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(159, 238);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 23);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Start";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(159, 267);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 23);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "Stop";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Version 1.1.2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "BotetehLabs";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 306);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Keybinder Solution";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Timer timer1;
+        private Label label3;
+        private Label label4;
+    }
+
+}
+
---
+++ Source code/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Keybind_Solution
+{
+    public partial class Form1 : Form
+    {
+        // Variables for storing each key
+        private Keys? Hotkey = null;
+        private Keys? Key1 = null;
+        private Keys? Key2 = null;
+        private Keys? Key3 = null;
+
+        private bool isRecording = false;
+        private Button activeButton;
+        private TextBox activeTextBox;
+        private bool hotkeyPressed = false;
+        private bool isActive = false; // Track if the program is active
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            button1.Text = "Record Hotkey";
+            button2.Text = "Record Key #1";
+            button3.Text = "Record Key #2";
+            button4.Text = "Record Key #3";
+
+            // Disable textboxes 1-4
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            textBox4.Enabled = false;
+            button8.Enabled = true;
+            button9.Enabled = false;
+
+            // Initialize textboxes to "NA" by default
+            textBox1.Text = "NA";
+            textBox2.Text = "NA";
+            textBox3.Text = "NA";
+            textBox4.Text = "NA";
+
+            // Set up the timer
+            Timer timer1 = new Timer();
+            timer1.Interval = 100; // Check every 100 ms
+            timer1.Tick += Timer_Tick;
+            timer1.Start();
+        }
+
+        // Button click event handlers
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartRecording(button1, textBox1);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            StartRecording(button2, textBox2);
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            StartRecording(button3, textBox3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StartRecording(button4, textBox4);
+        }
+
+        // Method to start/stop recording
+        private void StartRecording(Button button, TextBox textBox)
+        {
+            if (!isRecording)
+            {
+                isRecording = true;
+                activeButton = button;
+                activeTextBox = textBox;
+                button.Text = "Cancel";
+                textBox.Text = "Recording...";
+            }
+            else
+            {
+                // Cancel recording
+                isRecording = false;
+                ResetButtonAndTextBox(button, textBox, cancelled: true);
+            }
+        }
+
+        // Override ProcessCmdKey to capture key presses globally
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (isRecording && activeTextBox != null)
+            {
+                // Save the key to the corresponding variable based on the active button
+                if (activeButton == button1)
+                {
+                    Hotkey = keyData;
+                    textBox1.Text = Hotkey.ToString();
+                }
+                else if (activeButton == button2)
+                {
+                    Key1 = keyData;
+                    textBox2.Text = Key1.ToString();
+                }
+                else if (activeButton == button3)
+                {
+                    Key2 = keyData;
+                    textBox3.Text = Key2.ToString();
+                }
+                else if (activeButton == button4)
+                {
+                    Key3 = keyData;
+                    textBox4.Text = Key3.ToString();
+                }
+
+                isRecording = false;
+                ResetButtonAndTextBox(activeButton, activeTextBox, cancelled: false);
+                return true;
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        // Check for mouse clicks for recording
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            if (isRecording && activeTextBox != null)
+            {
+                if (activeButton == button1)
+                {
+                    Hotkey = (e.Button == MouseButtons.Left) ? Keys.LButton : Keys.RButton;
+                    textBox1.Text = Hotkey.ToString();
+                }
+                else if (activeButton == button2)
+                {
+                    Key1 = (e.Button == MouseButtons.Left) ? Keys.LButton : Keys.RButton;
+                    textBox2.Text = Key1.ToString();
+                }
+                else if (activeButton == button3)
+                {
+                    Key2 = (e.Button == MouseButtons.Left) ? Keys.LButton : Keys.RButton;
+                    textBox3.Text = Key2.ToString();
+                }
+                else if (activeButton == button4)
+                {
+                    Key3 = (e.Button == MouseButtons.Left) ? Keys.LButton : Keys.RButton;
+                    textBox4.Text = Key3.ToString();
+                }
+
+                isRecording = false;
+                ResetButtonAndTextBox(activeButton, activeTextBox, cancelled: false);
+                return;
+            }
+
+            base.OnMouseDown(e);
+        }
+
+        // Method to reset button and textbox to original state
+        private void ResetButtonAndTextBox(Button button, TextBox textBox, bool cancelled)
+        {
+            if (button == button1)
+            {
+                button.Text = "Record Hotkey";
+            }
+            else if (button == button2)
+            {
+                button.Text = "Record Key #1";
+            }
+            else if (button == button3)
+            {
+                button.Text = "Record Key #2";
+            }
+            else if (button == button4)
+            {
+                button.Text = "Record Key #3";
+            }
+
+            if (cancelled)
+            {
+                textBox.Text = "NA";
+
+                if (button == button1) { Hotkey = null; }
+                if (button == button2) { Key1 = null; }
+                if (button == button3) { Key2 = null; }
+                if (button == button4) { Key3 = null; }
+            }
+
+            activeButton = null;
+            activeTextBox = null;
+        }
+
+        // Clear Key1 and reset textBox2
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Key1 = null;
+            textBox2.Text = "NA";
+        }
+
+        // Clear Key2 and reset textBox3
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Key2 = null;
+            textBox3.Text = "NA";
+        }
+
+        // Clear Key3 and reset textBox4
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Key3 = null;
+            textBox4.Text = "NA";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (Hotkey == null)
+            {
+                MessageBox.Show("Hotkey cannot be null.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            isActive = true; // Set to true when starting
+            button8.Enabled = false;
+            button9.Enabled = true;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            isActive = false; // Set to false when stopping
+            button9.Enabled = false;
+            button8.Enabled = true;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (!isActive) // Check if the program is active
+            {
+                return; // Do nothing if not active
+            }
+
+            // Check for hotkey activation
+            if (Hotkey.HasValue && GetAsyncKeyState((int)Hotkey.Value) < 0 && !hotkeyPressed)
+            {
+                hotkeyPressed = true;
+
+                // Create an array for keys to press together
+                List<Keys> keysToPress = new List<Keys>();
+
+                if (Key1.HasValue) keysToPress.Add(Key1.Value);
+                if (Key2.HasValue) keysToPress.Add(Key2.Value);
+                if (Key3.HasValue) keysToPress.Add(Key3.Value);
+
+                // Send keys together
+                if (keysToPress.Count > 0)
+                    SendKeysTogether(keysToPress.ToArray());
+            }
+            else if (hotkeyPressed && GetAsyncKeyState((int)Hotkey.Value) == 0)
+            {
+                hotkeyPressed = false;
+            }
+        }
+
+        // Helper method to send keys together
+        private void SendKeysTogether(Keys[] keys)
+        {
+            foreach (var key in keys)
+            {
+                keybd_event((byte)key, 0, 0, 0); // Key down
+            }
+
+            foreach (var key in keys)
+            {
+                keybd_event((byte)key, 0, 2, 0); // Key up
+            }
+        }
+
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
+
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+
+    }
+}
